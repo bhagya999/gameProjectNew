86d8903bd5f5cf4571f0810b1b4ccc9e8b3dd2b9
bhagya <bhagyashiranakalankacrazy999@gmail.com>
2018-11-20 06:54:28 +0530

    Create snake-game.component.ts

---
+++ src/app/components/snake-game/snake-game.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+declare var snake:any;
+@Component({
+  selector: 'app-snake-game',
+  templateUrl: './snake-game.component.html',
+  styleUrls: ['./snake-game.component.css']
+})
+export class SnakeGameComponent implements OnInit {
+
+  constructor() {
+    
+   }
+
+  ngOnInit() {
+  }
+
+}
